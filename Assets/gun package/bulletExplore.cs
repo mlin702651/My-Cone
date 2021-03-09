8428e3e3ee4b10187a2fea38fd1a1b1b7f569e28
Ding <mlin702651@gmail.com>
2021-03-09 17:09:40 +0800

    gun

---
+++ Assets/gun package/bulletExplore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Pool;
+
+
+public class bulletExplore : MonoBehaviour
+{
+    public GameObject exp1;
+   
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject particle1;
+        particle1=Instantiate(exp1, transform.position, transform.rotation);
+        Destroy(particle1, 1);
+        Lean.Pool.LeanPool.Despawn(gameObject);
+
+    }
+}
---
+++ Assets/gun package/bulletcontroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Pool;
+
+
+public class bulletcontroller : MonoBehaviour
+{
+    public GameObject firePoint;
+    public Rigidbody bullet;
+    public float bulletSpeed=10.0f;
+
+    Vector3 bulletPos ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            //創造一個子彈
+            Rigidbody clone1;
+            clone1= Lean.Pool.LeanPool.Spawn(bullet,transform.position, Quaternion.identity) as Rigidbody;
+            clone1.velocity= transform.TransformDirection(Vector3.left* bulletSpeed);//讓子彈飛
+
+            //clone1.AddForce(-transform.right * bulletSpeed, ForceMode.VelocityChange);
+            
+        }
+        
+    }
+
+   
+}

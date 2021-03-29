5f3e21d265b3378053f54f350cb88f49acbf4d12
Ding <mlin702651@gmail.com>
2021-03-30 01:02:58 +0800

    switch camera

---
+++ Assets/chaio/ChangeCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject aimCamera;
+    //public GameObject aimReticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            mainCamera.SetActive(false);
+            aimCamera.SetActive(true);
+        }
+        else
+        {
+            mainCamera.SetActive(true);
+            aimCamera.SetActive(false);
+
+        }
+    }
+}
--- Assets/chaio/ThreePersonMovement.cs
+++ Assets/chaio/ThreePersonMovement.cs
@@ -6,7 +6,7 @@ public class ThreePersonMovement : MonoBehaviour
 {
 
     public CharacterController controller;
-    public Transform cam;
+    public Transform mainCam;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity ;
@@ -25,12 +25,14 @@ public class ThreePersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg+cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg+ mainCam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+
     }
 }

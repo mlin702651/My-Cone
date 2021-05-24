0eb7845778d746194272e9d72f305bde6d13d4ab
Ding <mlin702651@gmail.com>
2021-05-24 21:54:34 +0800

    magic 01+02

--- Assets/gun package/bulletcontroller.cs
+++ Assets/gun package/bulletcontroller.cs
@@ -7,26 +7,91 @@ using Lean.Pool;
 public class bulletcontroller : MonoBehaviour
 {
     public GameObject firePoint;
-    public Rigidbody bullet;
-    public float bulletSpeed=10.0f;
+    float timer = 0;
+    float flyTime = 2;
+    //續力
+    public GameObject flashEffect01;
+    public Rigidbody bullet01;
+    public float bulletSpeed01=10.0f;
 
-    Vector3 bulletPos ;
+    bool Flag = false;
+    float pressTime=0;
 
-    // Update is called once per frame
+    //泡泡
+    public GameObject flashEffect02;
+    public Rigidbody bullet02;
+    public float bulletSpeed02 = 10.0f;
+    
+
     void Update()
     {
-        if (Input.anyKeyDown)
-        {
-            //創造一個子彈
-            Rigidbody clone1;
-            clone1= Lean.Pool.LeanPool.Spawn(bullet,transform.position, Quaternion.identity) as Rigidbody;
-            clone1.velocity= transform.TransformDirection(Vector3.left* bulletSpeed);//讓子彈飛
-
-            //clone1.AddForce(-transform.right * bulletSpeed, ForceMode.VelocityChange);
-            
-        }
-        
+        magic01();
+        magic02();
     }
 
-   
+    void magic01()
+    {
+     
+        if (Input.GetKey("q")&& pressTime<=1.0f)
+        {
+            pressTime += Time.deltaTime;
+           
+        }
+        if (pressTime < 0.3f)
+        {
+            flyTime = 0.2f;
+        }
+        else
+        {
+            flyTime = Mathf.Pow(2, pressTime) - 1.0f;
+        }
+       
+        if (Input.GetKeyUp("q"))
+        {
+            Flag = true;
+            GameObject cloneFlash;
+            cloneFlash = Lean.Pool.LeanPool.Spawn(flashEffect01, transform.position, Quaternion.identity);
+            Lean.Pool.LeanPool.Despawn(cloneFlash, 2);
+
+            Rigidbody clonebullet;
+            clonebullet = Lean.Pool.LeanPool.Spawn(bullet01, transform.position, Quaternion.identity) as Rigidbody;
+            clonebullet.velocity = transform.TransformDirection(Vector3.left * bulletSpeed01);//讓子彈飛
+            Lean.Pool.LeanPool.Despawn(clonebullet, flyTime);
+
+            pressTime = 0;
+                     
+        }
+    }
+
+    void magic02()
+    {
+        if (Input.GetKey("w"))
+        {
+            Debug.Log(timer);
+            if (timer > 0.5f)
+            {
+                Debug.Log(timer);
+                GameObject cloneFlash;
+                cloneFlash = Lean.Pool.LeanPool.Spawn(flashEffect02, transform.position, Quaternion.identity);
+                Lean.Pool.LeanPool.Despawn(cloneFlash, 1);
+                Rigidbody clonebullet;
+                clonebullet = Lean.Pool.LeanPool.Spawn(bullet02, transform.position, Quaternion.identity) as Rigidbody;
+                clonebullet.velocity = transform.TransformDirection(Vector3.left * bulletSpeed02);//讓子彈飛
+                Lean.Pool.LeanPool.Despawn(clonebullet, 1);
+                timer = 0;
+            }
+            else timer += Time.deltaTime;         
+        }    
+    }
 }
+
+//Lean.Pool.LeanPool.Despawn(gameObject);
+
+//GameObject cloneFlash;
+//cloneFlash = Lean.Pool.LeanPool.Spawn(flashEffect02, transform.position, Quaternion.identity);
+//Lean.Pool.LeanPool.Despawn(cloneFlash, 2);
+
+//Rigidbody clonebullet;
+//clonebullet = Lean.Pool.LeanPool.Spawn(bullet01, transform.position, Quaternion.identity) as Rigidbody;
+//clonebullet.velocity = transform.TransformDirection(Vector3.left * bulletSpeed02);//讓子彈飛
+//Lean.Pool.LeanPool.Despawn(clonebullet, 1);

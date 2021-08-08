95006db556ac3874e9dfaa0e5c8186ec9216f67c
Ding <mlin702651@gmail.com>
2021-08-08 20:03:06 +0800

    change camera input

--- Assets/chaio/scripts/wallTransparentFollowPlayer.cs
+++ Assets/chaio/scripts/wallTransparentFollowPlayer.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class wallTransparentFollowPlayer : MonoBehaviour
 {
-    public int posID=Shader.PropertyToID("_Position");
-    public int sizeID=Shader.PropertyToID("_Size");
+    //取得material 變數的Id
+    int posID=Shader.PropertyToID("_Position");
+    int sizeID=Shader.PropertyToID("_Size");
 
     public Material wallMaterial;
     public Camera cerrentCamera;
     public LayerMask mask;
 
+    //用來調整圓中心點
     [SerializeField]
     Vector3 adjustPos=new Vector3(0,0,0);
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -27,6 +22,7 @@ public class wallTransparentFollowPlayer : MonoBehaviour
         var dir = cerrentCamera.transform.position-transform.position;
         var ray =new Ray(transform.position,dir.normalized);
 
+        //用一個射線偵測有沒有打到牆壁 
         if(Physics.Raycast(ray,3000,mask))
         {
             wallMaterial.SetFloat(sizeID,1.5f);
@@ -35,7 +31,8 @@ public class wallTransparentFollowPlayer : MonoBehaviour
         {
             wallMaterial.SetFloat(sizeID,0);
         }
-
+        
+        //把位子丟給material
         var view = cerrentCamera.WorldToViewportPoint(transform.position)+ adjustPos;
         wallMaterial.SetVector(posID,view);
     }

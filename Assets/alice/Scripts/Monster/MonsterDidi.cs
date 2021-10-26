4aa94ca9525611f494900e96734fb88c2737ee91
s110719005 <48974204+s110719005@users.noreply.github.com>
2021-10-27 05:35:02 +0800

    ADD didi pop out status

--- Assets/alice/Scripts/Monster/MonsterDidi.cs
+++ Assets/alice/Scripts/Monster/MonsterDidi.cs
@@ -9,16 +9,44 @@ public class MonsterDidi : MonoBehaviour
     public MonsterProfile monsterProfile;
     private NavMeshAgent _agent;
 
+    [SerializeField]private float underGroundOffset = -0.14f;
+    [SerializeField]private float onGroundOffset = 0.1f;
+    private float offsetInterpolation = 0;
+
+    #region Animation
+    [SerializeField]private bool isAttacked = false;
+    [SerializeField]private bool ifStartPopOut = false;
+    [SerializeField]private bool ifStartFall = false;
+    private Animator _didiAnimator;
+    private int currentAnimationState;
+
+    private int animationIdle;
+    private int animationPopOut;
+    private int animationRun;
+
+    [SerializeField]private float didiPopOutDelay;
+
+    #endregion
+
     [SerializeField] private GameObject Player;
     [SerializeField] private GameObject Goal;
 
     [SerializeField] private float DidiDistanceRun = 4.0f;
     [SerializeField] private float DidiDistanceGoal = 8.0f;
     
+    
+    private void Awake() {
+        _didiAnimator = GetComponent<Animator>();
+        animationIdle = Animator.StringToHash("MonsterDiDi_Idle");
+        animationPopOut = Animator.StringToHash("MonsterDiDi_PopOut");
+        animationRun = Animator.StringToHash("MonsterDiDi_Run");
+    }
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _agent.baseOffset = underGroundOffset;
+        ChangeAnimationState(animationIdle);
     }
 
     // Update is called once per frame
@@ -29,15 +57,62 @@ public class MonsterDidi : MonoBehaviour
 
         Debug.Log("Distance" + distanceToPlayer);
 
+        if(isAttacked){
+            isAttacked = false;
+            PopOut();
+        }
+        if(ifStartPopOut){
+            _agent.baseOffset = Mathf.Lerp(underGroundOffset,onGroundOffset,offsetInterpolation);
+            offsetInterpolation += Time.deltaTime/0.3f;
+            if(offsetInterpolation>=1) {
+                ifStartPopOut = false;
+                ifStartFall = true;
+            }
+        }
+        if(ifStartFall){
+            _agent.baseOffset = Mathf.Lerp(0,onGroundOffset,offsetInterpolation);
+            offsetInterpolation -= Time.deltaTime/0.3f;
+            if(offsetInterpolation<=0) ifStartFall = false;
+        }
+        if(currentAnimationState==animationPopOut) return;
         if(distanceToGoal < DidiDistanceGoal){
+            ChangeAnimationState(animationRun);
             Vector3 dirToGoal = transform.position - Goal.transform.position;
             Vector3 newPos = transform.position - dirToGoal;
             _agent.SetDestination(newPos);
-        }
+        } 
         else if(distanceToPlayer < DidiDistanceRun){
+            ChangeAnimationState(animationRun);
             Vector3 dirToPlayer = transform.position - Player.transform.position;
             Vector3 newPos = transform.position + dirToPlayer;
             _agent.SetDestination(newPos);
         }
+        else ChangeAnimationState(animationIdle);
+
+    }
+
+    void ChangeAnimationState(int newAnimationState)
+    {
+       if(newAnimationState == currentAnimationState) {
+            return; //一樣的話就不重新開始播ㄌ
+        }
+        _didiAnimator.Play(newAnimationState);
+        
+        currentAnimationState = newAnimationState;
+    }
+
+    void PopOut(){
+        if(offsetInterpolation>=1) return;
+        ChangeAnimationState(animationPopOut);
+        //didiPopOutDelay = _didiAnimator.GetCurrentAnimatorStateInfo(0).length;
+        //didiPopOutDelay = _didiAnimator.Get
+        ifStartPopOut = true;
+        Invoke("PlayDidiIdleAnimation",didiPopOutDelay);
+
+
+    }
+
+    void PlayDidiIdleAnimation(){
+        ChangeAnimationState(animationIdle);
     }
 }

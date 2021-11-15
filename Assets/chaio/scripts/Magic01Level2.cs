a6b102b4e9d4fb05775232b8297a11d155d6feee
Ding <mlin702651@gmail.com>
2021-11-16 00:46:19 +0800

    training01 scale

---
+++ Assets/chaio/scripts/Magic01Level2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic01Level2 : MonoBehaviour
+{
+    [SerializeField]float speed=0.5f;
+    float timer=0;
+    bool start=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer+=Time.deltaTime;
+        if(start){
+             transform.position+=transform.forward*speed*Time.deltaTime;
+        }
+       
+       
+        
+    }
+     void OnTriggerEnter(Collider other)
+        {
+            start=true;
+        }
+}

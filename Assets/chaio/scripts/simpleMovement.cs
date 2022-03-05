caeb17f25678414164c6915e7a916ac97b0ba4e1
mlin702651 <55220325+mlin702651@users.noreply.github.com>
2022-03-05 20:52:10 +0800

    change kirakirarock mushroom particle

---
+++ Assets/chaio/scripts/simpleMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleMovement : MonoBehaviour
+{
+    [SerializeField]float speed=0.5f;
+    bool isgrounded=false;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //if(!isgrounded)
+        rb.AddForce(-transform.up*speed);
+        //rb.velocity=transform.forward*speed;
+        //transform.Translate(-transform.up*speed*Time.deltaTime);
+    }
+}

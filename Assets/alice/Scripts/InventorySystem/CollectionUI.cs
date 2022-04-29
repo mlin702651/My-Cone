cc6693611a2ba69ecd1597314723595680f2c7ed
LAPTOP-A9JPSQQJ\Alice <alicewang6538@gmail.com>
2022-04-30 05:07:18 +0900

    ADD collection UI & function

---
+++ Assets/alice/Scripts/InventorySystem/CollectionUI.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CollectionUI : MonoBehaviour
+{
+    public InventorySlot[] slots;
+    public GameObject parentUI;
+    
+    
+
+    private void Start() {
+        slots = GetComponentsInChildren<InventorySlot>();
+        parentUI.SetActive(false);
+    }
+    private int? GetNextEmptySlot(){
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if(slots[i].slotItem == null) return i;
+        }
+        return null;
+    }
+    private int? GetSameSlot(ItemBase item){
+        for (int i = slots.Length-1; i >= 0 ; i--)
+        {
+            if(slots[i].slotItem != null) {
+                if(slots[i].slotItem == item) return i;
+            }
+        }
+        return null;
+    }
+    public void UpdateInventoryAdd(ItemBase item){
+        var remainder = GetRemainder(item);
+
+        if(remainder == 0){
+            remainder = item.maxStackSize;
+        }
+
+        if(remainder ==1){
+            print("add slot");
+            slots[(int)GetNextEmptySlot()].AddItem(item);
+            if(item is HatProp){ //如果是帽子幫她註冊一個可以戴帽子的事件
+                HatProp hatProp = item as HatProp;
+                slots[(int)GetSameSlot(item)].GetComponent<UnityItemEventHandler>().unityEvent = hatProp.itemEvent;
+            }
+
+        }
+        else{
+            slots[(int)GetSameSlot(item)].amount.text = remainder.ToString();
+        }
+
+        //Debug.Log("UIAdditem");
+    }
+
+    public void UpdateInventoryRemove(ItemBase item){
+        var remainder = GetRemainder(item);
+
+        if(remainder == 0){
+            remainder = item.maxStackSize;
+        }
+        if(remainder == item.maxStackSize){
+            slots[(int)GetSameSlot(item)].amount.text = "";
+            slots[(int)GetSameSlot(item)].RemoveItem();
+        }
+        else{
+            slots[(int)GetSameSlot(item)].amount.text = remainder.ToString();
+        }
+    }
+    public void UpdateInventorySelection(int newSelection,int currentSelection){
+        slots[currentSelection].UpdateFrame(false);
+        slots[newSelection].UpdateFrame(true);
+        // for (int i = 0; i < slots.Length; i++)
+        // {
+        //     if(i == currentSelection) slots[i].UpdateFrame(true);
+        //     else slots[i].UpdateFrame(false);
+        // }
+        
+    }
+    
+    public void ResetInventoryUI(int currentSelection){
+        slots[currentSelection].UpdateFrame(false);
+    }
+
+    private int GetRemainder(ItemBase newItem){
+        var itemCount = InventoryManager.instance.collection.Count(x => x== newItem); //傳回背包裡 這種newItem的數量
+        return itemCount % newItem.maxStackSize;
+    }
+
+    private ItemBase GetSelectedItem(int newSelection){
+        return slots[newSelection].slotItem;
+    }
+
+    private void OpenEquipMenu(int currentSelection){
+        slots[currentSelection].OpenEquipWindow();
+    }
+
+    private void CheckEquipItem(int currentSelection){
+        slots[currentSelection].EquipItem();
+    }
+}
---
+++ Assets/alice/Scripts/UI/CollectionMenuManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionMenuManager : MonoBehaviour
+{
+    [SerializeField]public GameObject CollectionMenuUI;
+    [SerializeField]private CollectionUI collectionUI;
+    private int currentSelection = 0;
+    private int newSelection = 0;
+    private ItemBase currentSelectedItem;
+
+
+    [SerializeField]private Image selectedItemImage;
+    [SerializeField]private Text selectedItemName;
+    [SerializeField]private Text selectedItemDescription;
+    
+    private void Start() {
+        InventoryManager.instance.onMenuPageChangeCallBack += CheckOpenMenu; 
+        InventoryManager.instance.onCollectionAddCallBack += AddItem;
+        //CollectionMenuUI.SetActive(false);
+
+    }
+    private void Update() {
+        if(CollectionMenuUI.activeSelf){
+            HandleSelection();
+        }
+    }
+
+    private void AddItem(ItemBase newItem){
+        collectionUI.UpdateInventoryAdd(newItem);
+    }
+    
+    void HandleSelection(){
+        //if(InventoryManager.instance.currentMenuPage!=0) return;
+        if(InventoryManager.instance.collection.Count <=0) return;
+        
+        if(InputSystem.instance.MenuSelectDownPressDown){ //按往下的鈕
+            InputSystem.instance.MenuSelectDownPressDown = false;
+            Debug.Log("down");
+            newSelection = (currentSelection+3<InventoryManager.instance.collection.Count)?currentSelection+3:currentSelection; //如果有下一排 選下面那個
+            UpdateSelection();
+        }
+        else if(InputSystem.instance.MenuSelectUpPressDown){
+            InputSystem.instance.MenuSelectUpPressDown = false;
+            Debug.Log("up");
+            newSelection = (currentSelection-3>=0)?currentSelection-3:currentSelection; //如果有上一排 選上面那個
+            UpdateSelection();
+        }
+        else if(InputSystem.instance.MenuSelectRightPressDown){
+            InputSystem.instance.MenuSelectRightPressDown = false;
+            Debug.Log("Right");
+            newSelection = ((currentSelection+1)<InventoryManager.instance.collection.Count)?currentSelection+1:currentSelection; //如果有上一排 選上面那個
+            UpdateSelection();
+        }
+        else if(InputSystem.instance.MenuSelectLeftPressDown){
+            InputSystem.instance.MenuSelectLeftPressDown = false;
+            Debug.Log("Left");
+            newSelection = ((currentSelection-1)>=0)?currentSelection-1:currentSelection; //如果有上一排 選上面那個
+            UpdateSelection();
+        }
+
+        
+    }
+
+    void UpdateSelection(){
+        if(InventoryManager.instance.collection.Count <=0) return;
+        //onSelectionChangeCallBack.Invoke(newSelection,currentSelection);
+        //currentSelectedItem = getCurrentItemCallBack.Invoke(newSelection);
+        currentSelectedItem = InventoryManager.instance.collection[newSelection];
+        selectedItemImage.overrideSprite = currentSelectedItem.itemIcon;
+        selectedItemName.text = currentSelectedItem.itemName;
+        selectedItemDescription.text = currentSelectedItem.itemDescription;
+        print(newSelection + "/" +currentSelection);
+        collectionUI.UpdateInventorySelection(newSelection,currentSelection);
+        currentSelection = newSelection;
+    }
+    public void CheckOpenMenu(int newMenuPage){
+        if(newMenuPage == 2){
+            CollectionMenuUI.SetActive(true);
+            if(InventoryManager.instance.collection.Count != 0){
+                currentSelectedItem = InventoryManager.instance.collection[0];
+                newSelection = 0;
+                currentSelection = 0;
+                UpdateSelection();
+            }
+        }
+        else CollectionMenuUI.SetActive(false);
+    }
+}

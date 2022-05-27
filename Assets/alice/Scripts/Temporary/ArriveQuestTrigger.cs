081e8ebce7852deae60d171b56d29f1423e83a9d
LAPTOP-A9JPSQQJ\Alice <alicewang6538@gmail.com>
2022-05-28 00:08:30 +0900

    推一下

---
+++ Assets/alice/Scripts/Temporary/ArriveQuestTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArriveQuestTrigger : MonoBehaviour
+{
+    [SerializeField]private string targetName;
+    
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player"){
+            GameManager.instance.onPlayerArrivedCallBack?.Invoke(targetName);
+            Destroy(gameObject);
+        }
+    }
+}

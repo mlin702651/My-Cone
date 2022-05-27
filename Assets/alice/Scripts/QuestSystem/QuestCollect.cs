f832e5a8bc5105188af8a21ac66e359f7697034d
LAPTOP-A9JPSQQJ\Alice <alicewang6538@gmail.com>
2022-05-28 01:18:04 +0900

    串到支線開始之前

---
+++ Assets/alice/Scripts/QuestSystem/QuestCollect.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Collect Quest", menuName = "Quests/Collect Quest")]
+public class QuestCollect : QuestBase
+{
+    [System.Serializable]
+    public class Objective{
+        public CollectableProfile requiredCollectable;
+        public int requiredAmount;
+    }
+    public Objective[] objectives;
+
+    public override void InitializedQuest(){
+
+        RequiredAmount = new int[objectives.Length];
+
+        for(int i=0;i< objectives.Length;i++){
+            RequiredAmount[i] = objectives[i].requiredAmount;
+        }
+
+        GameManager.instance.onEnemyDeathCallBack += MonsterDeath;
+        base.InitializedQuest();
+    }
+
+    private void MonsterDeath(MonsterProfile slaintMonster){
+        for(int i = 0; i < objectives.Length; i++){
+            if(slaintMonster == objectives[i].requiredCollectable){
+                CurrentAmount[i]++;
+                //更新給UI
+                //QuestManager.instance.UpdateQuestTracker($"{questDescription + "    " + CurrentAmount[i] + "/" + RequiredAmount[i]}");
+                QuestManager.instance.UpdateQuestTracker(this.GetObjectiveList());
+            }
+        }
+
+        Evaluate();
+    }
+
+    public override string GetObjectiveList(){
+        
+        string tempObjectiveList = "";
+
+        for (int i = 0; i < objectives.Length; i++)
+        {
+            tempObjectiveList += $"已完成{"    ( " + CurrentAmount[i] + " / " + RequiredAmount[i]+ " ) 隻" + objectives[i].requiredCollectable.collectableName} \n";
+        }
+
+        return tempObjectiveList;
+    }
+}
